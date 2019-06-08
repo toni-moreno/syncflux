a966d8f06d5dbc3397d8702c6ced4204188d6872
toni-moreno <toni.moreno@gmail.com>
2019-06-08 06:40:26 +0200

    added suport for unix timestamp  values on -start and -end parameters

--- pkg/util.go
+++ pkg/util.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 	"time"
 )
@@ -8,6 +9,9 @@ import (
 func parseInputTime(t string) (time.Time, error) {
 	//check if a negative duration.
 	var T time.Time
+	if i, err := strconv.ParseInt(t, 10, 32); err == nil {
+		return time.Unix(i, 0), nil
+	}
 	if strings.HasPrefix(t, "-") {
 		//parse as duration
 		dur, err := time.ParseDuration(t)
